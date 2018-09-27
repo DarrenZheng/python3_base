3b93e927e84e5f6bf2d9558ac714b547e7906e20
zhengda <zhengda@sztozed.net>
2018-09-27 22:01:51 +0800

    this is simple param define

---
+++ firstday/param.py
@@ -0,0 +1,10 @@
+i = 1
+print(i)
+
+i = i + 5
+print(i)
+
+s = '''hello work
+d'''
+
+print(s)
---
+++ firstday/str.py
@@ -0,0 +1,9 @@
+print("hello  12 12   43'\
+      dfdfdf")
+
+print('23232323  232 232"\n\
+      ddfdf');
+
+print('''dsfdfdf dfdf 
+dfdf 
+dfdf''');

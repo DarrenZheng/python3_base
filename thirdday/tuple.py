4ae72bc9d4c6f98593c8a134bf696181cf7c7d4e
zhengda <zhengda@sztozed.net>
2018-10-17 12:58:31 +0800

    this is simple tuple

---
+++ thirdday/tuple.py
@@ -0,0 +1,8 @@
+zoo = ('wolf', 'elephant', 'penguin')
+print("number of animals in the zoo is", len(zoo))
+print('they are', zoo)
+
+new_zoo=('monkey', 'dolphin', zoo)
+print('new_zoo has',len(new_zoo),'animals, there are', new_zoo)
+
+print(new_zoo[2][0])

6674d03b9ec9804e065706160605fa4396af0542
zhengda <zhengda@sztozed.net>
2018-10-17 12:44:45 +0800

    print 不换行输出

--- thirdday/list.py
+++ thirdday/list.py
@@ -2,7 +2,7 @@ shoplist = ['apple', 'mango', 'carrno']
 print("i have", len(shoplist), 'items to purchase.')
 print("these item are"),
 for item in shoplist:
-    print(item,),
+    print(item, end=' ')
 
 print('\nIalso havve buy rice')
 shoplist.append('rice')

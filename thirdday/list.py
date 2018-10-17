ac52829de080b4512ab22b724befd653994d6a20
zhengda <zhengda@sztozed.net>
2018-10-17 12:50:28 +0800

    使用list的排序，del函数

--- thirdday/list.py
+++ thirdday/list.py
@@ -8,3 +8,13 @@ print('\nIalso havve buy rice')
 shoplist.append('rice')
 
 print("now list is",shoplist)
+
+shoplist.sort()
+print('after sort, list is',shoplist)
+
+print('the first item i want to buy is', shoplist[0])
+#del shoplist[0]
+
+shoplist.__delitem__(-1)
+
+print('my shopping list now is: ', shoplist)

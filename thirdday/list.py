faa84deb578aa2d65ffe164bbc99c499968b56db
zhengda <zhengda@sztozed.net>
2018-10-17 12:43:13 +0800

    this is simple list

---
+++ thirdday/list.py
@@ -0,0 +1,10 @@
+shoplist = ['apple', 'mango', 'carrno']
+print("i have", len(shoplist), 'items to purchase.')
+print("these item are"),
+for item in shoplist:
+    print(item,),
+
+print('\nIalso havve buy rice')
+shoplist.append('rice')
+
+print("now list is",shoplist)

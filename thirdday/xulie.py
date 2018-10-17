1f536966b71089ecf08029354a200645e4fcb69e
zhengda <zhengda@sztozed.net>
2018-10-17 14:28:56 +0800

    对列表的简单切片

---
+++ thirdday/xulie.py
@@ -0,0 +1,16 @@
+shoplist = ['apple', 'mango', 'carrot', 'banana']
+
+print('item 0 is', shoplist[0])
+print('item 1 is', shoplist[1])
+print('item 2 is', shoplist[2])
+print('item 3 is', shoplist[3])
+print('item -1 is', shoplist[-1])
+print('item -2 is', shoplist[-2])
+
+print('item 1 to 3 is', shoplist[1:3])
+print('item 1 to end is', shoplist[1:])
+print('item 1 to -1 is', shoplist[1:-1])
+print('item -1 to 1 is', shoplist[-1:1])
+print('item start to end is', shoplist[:])
+
+

91a3f791e17c2b0a624364e82de9ef0157c3216f
zhengda <zhengda@sztozed.net>
2018-10-20 01:20:59 +0800

    备份3，比备份2多了压缩文件名可带提交内容

--- fourthday/backup2.py
+++ fourthday/backup2.py
@@ -8,12 +8,21 @@ source = ['/tmp/test/source/onlyfile/a.txt', '/tmp/test/source/dir']
 target_dir = '/tmp/test/backup/'
 
 today = target_dir + time.strftime('%Y-%m-%d')
-now = today + "/" + time.strftime('%Hhours-%Mmins-%Sseconds') +'.zip'
+now = ''
 
 print('today =', today)
 print('now = ', now)
 
 os.system('mkdir -p ' + today)
+
+comment = input("do you want to say something?")
+
+if len(comment) == 0:
+    now = today + "/" + time.strftime('%Hhours-%Mmins-%Sseconds') + '.zip'
+else:
+    now = today + "/" + time.strftime('%Hhours-%Mmins-%Sseconds') + \
+          '_' + comment + \
+          '.zip'
 
 zip_cmd = "zip -qr '%s' %s" %(now, ' '.join(source))
 
